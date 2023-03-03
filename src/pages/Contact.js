e093eecbc97b1896bf2de56ec06ffa862681968b
Jezebel <dg.jezebel@gmail.com>
2023-03-02 21:34:58 -0500

    tried to fix background in contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,24 +8,18 @@ import fb from '../Images/fbpng.png'
 export default class Contact extends React.Component {
     render(){
         return(
-        <div className='height'>
-            <div className='contactDiv'>
+            <div className='height'>
                 <div className='contactGreeting'>
                     <h2>Phone number:{' '}<a href="tel:215-792-2197" >(215) 792-2197</a></h2>
                     <h2>Email:{' '}<a href="mailto:customerservice@ab-catering.com">customerservice@ab-catering.com</a></h2>
                     <h2>Follow us on Facebook and Instagram </h2>
-                    <Link to='https://facebook.com/abcatering028072' target="_blank" rel="noopener noreferrer"><img className='iconssmall' src={fb}/></Link>
-                    <Link to='https://instagram.com/abcatering028072' target="_blank" rel="noopener noreferrer"><img className='iconssmall'src={insta}/></Link>
-                    <br/>
-                    
-            
-                <div className='formholder'>
-                </div>
-                
-                <ContactUs />
+                    <Link to='https://facebook.com/abcatering028072' target="_blank" rel="noopener noreferrer"><img className='iconssmall' alt='facebook'src={fb}/></Link>
+                    <Link to='https://instagram.com/abcatering028072' target="_blank" rel="noopener noreferrer"><img className='iconssmall'alt='instagram'src={insta}/></Link>
+                    <div className='formholder'>
+                    </div>
+                    <ContactUs />
                 </div>
             </div>
-        </div>
         )
     }
 }
