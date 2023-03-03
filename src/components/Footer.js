07cd1400e86e3f29fe6eeb42020fb3481a8d2169
Jezebel <dg.jezebel@gmail.com>
2023-03-02 20:59:39 -0500

    added facebook and instagram

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,5 +2,5 @@ import React from 'react'
 import '../css/footer.css'
 
 export default function(){
-    return <div className='footer'></div>
+    return <div className='footer'>Copyright AB-Catering 2023</div>
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {ContactUs} from '../components/Email'
 import '../css/email.css'
+import { Link } from 'react-router-dom';
+import insta from '../Images/instapng.png'
+import fb from '../Images/fbpng.png'
 
 export default class Contact extends React.Component {
     render(){
@@ -10,6 +13,9 @@ export default class Contact extends React.Component {
                 <div className='contactGreeting'>
                     <h2>Phone number:{' '}<a href="tel:215-792-2197" >(215) 792-2197</a></h2>
                     <h2>Email:{' '}<a href="mailto:customerservice@ab-catering.com">customerservice@ab-catering.com</a></h2>
+                    <h2>Follow us on Facebook and Instagram </h2>
+                    <Link to='https://facebook.com/abcatering028072' target="_blank" rel="noopener noreferrer"><img className='iconssmall' src={fb}/></Link>
+                    <Link to='https://instagram.com/abcatering028072' target="_blank" rel="noopener noreferrer"><img className='iconssmall'src={insta}/></Link>
                     <br/>
                     
             
