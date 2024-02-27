c99a632e41ebe8e9cefc5ffd44e45bd883b22982
Jezebel <dg.jezebel@gmail.com>
2024-02-26 20:37:39 -0500

    added photos to gallery

--- src/components/galleryDatabase.js
+++ src/components/galleryDatabase.js
@@ -1,3 +1,9 @@
+import image31 from '../GalleryImages/image31.jpg'
+import image32 from '../GalleryImages/image32.jpg'
+import image33 from '../GalleryImages/image33.jpg'
+import image34 from '../GalleryImages/image34.jpg'
+import image35 from '../GalleryImages/image35.jpg'
+
 import image1 from '../GalleryImages/image1.png'
 import image2 from '../GalleryImages/image2.png'
 import image3 from '../GalleryImages/image3.png'
@@ -33,7 +39,8 @@ image1, image2, image3, image4, image5, image6,
 image7, image8, image9, image10, image11, image12, 
 image13, image14, image15, image16, image17, image18, 
 image19, image20, image21, image22, image23, image24, 
-image25, image26, image27, image28, image29, image30
+image25, image26, image27, image28, image29, image30, image31,
+image32, image33, image34, image35,
 ]
 
 export {images}
